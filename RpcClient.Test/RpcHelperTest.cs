8f198d2fe50b1f6325e51d873903a04ab2e64178
ligl <ligl@jingzhengu.com>
2017-10-31 16:01:42 +0800

    添加rpchelper，test文件

---
+++ RpcClient.Test/RpcHelperTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RpcClient.Test
+{
+    [TestClass]
+    public class RpcHelperTest
+    {
+        /// <summary>
+        /// rpc get测试
+        /// </summary>
+        [TestMethod]
+        public void RpcGet()
+        {
+        }
+
+        /// <summary>
+        /// rpc post测试
+        /// </summary>
+        [TestMethod]
+        public void RpcPost()
+        {
+        }
+    }
+}
---
+++ RpcClient/RpcHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpcClient
+{
+    public class RpcHelper
+    {
+        #region get相关操作
+        #endregion
+
+        #region post相关操作
+        #endregion
+    }
+}
